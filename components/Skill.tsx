7dd24c3d623940a003c6c550e25bb40d48483359
Fabio Rodriguez <alessf13@gmail.com>
2023-05-30 14:04:29 +0100

    coloring changed for skills according to mode

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -30,7 +30,7 @@ export default function Skill({directionLeft, skill}: SkillProps) {
             transition duration-300 ease-in-out group-hover:bg-white h-24 w-24
             md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
                 <div className='flex items-center justify-center h-full'>
-                    <p className='text-3xl font-bold text-black opacity-100'>{skill.percentage.toString()}%</p>
+                    <p className='text-3xl font-bold text-violet-500 dark:text-orange-500'>{skill.percentage.toString()}%</p>
                 </div>
             </div>
         </div>
