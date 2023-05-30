d9f32b35daeed83221f6c41f8a7e1c8967bd1339
Fabio Rodriguez <alessf13@gmail.com>
2023-05-30 08:18:59 +0100

    image set up

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useTypewriter, Cursor } from "react-simple-typewriter"
 import BackgroundCircles from "./BackgroundCircles"
+import Image from "next/image"
 
 export default function Hero() {
     const [text, count] = useTypewriter({
@@ -16,6 +17,12 @@ export default function Hero() {
         <div className="h-screen flex flex-col space-y-8 items-center 
         justify-center text-center overflow-hidden">
             <BackgroundCircles />
+            {/* <Image 
+                src="https://avatars.githubusercontent.com/u/71491674?s=400&u=8e0bcb96298f7273738cf94a85b1f3d15126e580&v=4"
+                alt="Profile Picture of Fabio Rodriguez"
+                width={300}
+                height={300}
+            /> */}
             <h1>
                 <span>{text}</span>
                 <Cursor cursorColor="violet" />
