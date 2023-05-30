b3547c7f6b57e222a194c88f80e07986a774ca34
Fabio Rodriguez <alessf13@gmail.com>
2023-05-30 11:33:20 +0100

    skill component created and added to skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,12 +1,32 @@
 'use client'
 
 import {motion} from 'framer-motion'
+import Skill from './Skill'
 
 export default function Skills() {
     return(
-        <motion.div className="h-screen flex relative flex-col text-center md:text-left xl:flex-row
-        max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 items-center">
+        <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            
+            className="h-screen flex relative flex-col text-center md:text-left xl:flex-row
+                max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 items-center">
             <h3 className="top-24 uppercase tracking-[20px] text-2xl absolute">Skills</h3>
+
+            <div className='grid grid-cols-4 gap-5'>
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+            </div>
+        
         </motion.div>
     )
 }
