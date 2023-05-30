20a7561f1a8314f9102d432a86a620ff50cc81fb
Fabio Rodriguez <alessf13@gmail.com>
2023-05-30 14:18:44 +0100

    color for arrow

--- components/StickyNav.tsx
+++ components/StickyNav.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import arrow from "../public/assets/arrow.svg"
 
 export default function StickyNav(){
     return(
         <Link href="#hero">
            <footer className="sticky bottom-5 w-full cursor-pointer">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center ">
                 <Image 
-                    className="h-10 w-10 rounded-full filter grayscale
-                    hover:grayscale-0"
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" 
+                    className="h-10 w-10 rounded-full bg-violet-300 hover:bg-violet-500"
+                    src={arrow}
                     alt="Img that brings us back to the top" 
                     width={50}
                     height={50}
