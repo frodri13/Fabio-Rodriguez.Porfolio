d217edcfec6300e7547df28311a342b9438455c3
Fabio Rodriguez <alessf13@gmail.com>
2023-05-30 11:27:21 +0100

    initial styling for skills component

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import {motion} from 'framer-motion'
+
 export default function Skills() {
     return(
-        <div className="h-screen">
-            <h3 className="top-24 uppercase tracking-[20px] text-2xl text-center p-20">Skills</h3>
-        </div>
+        <motion.div className="h-screen flex relative flex-col text-center md:text-left xl:flex-row
+        max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 items-center">
+            <h3 className="top-24 uppercase tracking-[20px] text-2xl absolute">Skills</h3>
+        </motion.div>
     )
 }
