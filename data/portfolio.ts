7503fd8e54179cb83a9946ff9da90d9f8279cfe9
Fabio Rodriguez <alessf13@gmail.com>
2023-05-31 14:08:21 +0100

    added remaining paths

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -1,5 +1,12 @@
 import haiku from '/public/static/images/haiku-itter.png'
 import pinMyHike from '/public/static/images/PinMyHike.png' 
+import nico from '/public/static/images/photographer.png'
+import beat from '/public/static/images/beatmaker.png'
+import coloors from '/public/static/images/coloors.png'
+import bnb from '/public/static/images/MakersBnB.png'
+import aceBook from '/public/static/images/Acebook.png'
+import pPorfolio from '/public/static/images/old-porfolio.png'
+
 import { StaticImageData } from 'next/image'
 
 type Project = {
@@ -25,48 +32,48 @@ const projects: Project = [
         link: 'https://github.com/frank-mck/pin-my-hike',
         github: 'https://github.com/frank-mck/pin-my-hike',
     },
-    // {
-    //     title: 'Photogapher Portfolio',
-    //     imgUrl: '../public/assets/photographer.png',
-    //     stack: ['HTML', 'CSS', 'JavaScript'],
-    //     link: 'https://photomanifiesto.netlify.app/',
-    //     github: 'https://github.com/frodri13/Nicola-Rocco-Photographer',
-    // },
-    // {
-    //     title: 'BeatMaker',
-    //     imgUrl: '../public/assets/beatmaker.png',
-    //     stack: ['HTML', 'CSS', 'JavaScript'],
-    //     link: 'https://sad-khorana-46b20f.netlify.app/',
-    //     github: 'https://github.com/frodri13/Beatmaker',
-    // } ,
-    // {
-    //     title: 'Coloors',
-    //     imgUrl: '../public/assets/coloors.png',
-    //     stack: ['HTML', 'CSS', 'JavaScript'],
-    //     link: 'https://eager-liskov-372011.netlify.app/',
-    //     github: 'https://github.com/frodri13/Coloors-Project',
-    // },
-    // {
-    //     title: 'MakersBnB',
-    //     imgUrl: '../public/assets/MakersBnB.png',
-    //     stack: ['HTML', 'CSS', 'Ruby'],
-    //     link: 'https://github.com/frank-mck/MakerBnB',
-    //     github: 'https://github.com/frank-mck/MakerBnB',
-    // },
-    // {
-    //     title: 'AceBook',
-    //     imgUrl: '../public/assets/Acebook.png',
-    //     stack: ['HTML', 'SCSS', 'JavaScript','Ruby', 'Rails'],
-    //     link: 'https://github.com/frodri13/acebook-rails-soda/',
-    //     github: 'https://github.com/frodri13/acebook-rails-soda/',
-    // },
-    // {
-    //     title: "Previous Porfolio",
-    //     imgUrl: "../public/assets/old-porfolio.png",
-    //     stack: ["HTML", "CSS", "JavaScript"],
-    //     link: "https://fabio-rodriguez.netlify.app/",
-    //     github: 'https://github.com/frodri13/Portofolio'
-    // }
+    {
+        title: 'Photogapher Portfolio',
+        imgUrl: nico,
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://photomanifiesto.netlify.app/',
+        github: 'https://github.com/frodri13/Nicola-Rocco-Photographer',
+    },
+    {
+        title: 'BeatMaker',
+        imgUrl: beat,
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://sad-khorana-46b20f.netlify.app/',
+        github: 'https://github.com/frodri13/Beatmaker',
+    } ,
+    {
+        title: 'Coloors',
+        imgUrl: coloors,
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://eager-liskov-372011.netlify.app/',
+        github: 'https://github.com/frodri13/Coloors-Project',
+    },
+    {
+        title: 'MakersBnB',
+        imgUrl: bnb,
+        stack: ['HTML', 'CSS', 'Ruby'],
+        link: 'https://github.com/frank-mck/MakerBnB',
+        github: 'https://github.com/frank-mck/MakerBnB',
+    },
+    {
+        title: 'AceBook',
+        imgUrl: aceBook,
+        stack: ['HTML', 'SCSS', 'JavaScript','Ruby', 'Rails'],
+        link: 'https://github.com/frodri13/acebook-rails-soda/',
+        github: 'https://github.com/frodri13/acebook-rails-soda/',
+    },
+    {
+        title: "Previous Porfolio",
+        imgUrl: pPorfolio,
+        stack: ["HTML", "CSS", "JavaScript"],
+        link: "https://fabio-rodriguez.netlify.app/",
+        github: 'https://github.com/frodri13/Portofolio'
+    }
 ]
 
 export default projects
