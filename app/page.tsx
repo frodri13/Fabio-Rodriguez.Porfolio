b1a1c688dd60ce1360a336829bf376eee03e2182
Fabio Rodriguez <alessf13@gmail.com>
2023-05-31 19:40:08 +0100

    snap taken out

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main >
       <div >
-        <section id="hero" className="snap-center">
+        <section id="hero">
           <Hero />
         </section>
         <section id="about">
