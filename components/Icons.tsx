48dfe9d22767295b76b512f8ed51cedb33b08a1d
Fabio Rodriguez <alessf13@gmail.com>
2023-06-03 07:20:39 +0100

    target _blank to links

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -3,10 +3,10 @@ import { SocialIcon } from 'react-social-icons';
 export default function Icons(){
     return(
         <div className='flex flex-wrap justify-center gap-2'>
-            <SocialIcon url="https://www.linkedin.com/in/rodriguezfabio" fgColor='white' bgColor='gray' />               
-            <SocialIcon url='https://github.com/frodri13' fgColor='white' bgColor='gray'/>
-            <SocialIcon url='https://frontendmasters.com/u/frodri13/' fgColor='white' bgColor='gray'/>
-            <SocialIcon url="https://twitter.com/fabio50298777" fgColor='white' bgColor='gray'/>
+            <SocialIcon url="https://www.linkedin.com/in/rodriguezfabio" fgColor='white' bgColor='gray' target='_blank'/>               
+            <SocialIcon url='https://github.com/frodri13' fgColor='white' bgColor='gray' target='_blank'/>
+            <SocialIcon url='https://frontendmasters.com/u/frodri13/' fgColor='white' bgColor='gray' target='_blank'/>
+            <SocialIcon url="https://twitter.com/fabio50298777" fgColor='white' bgColor='gray' target='_blank'/>
             <SocialIcon url="https://www.instagram.com/fabsrocco/" fgColor='white' bgColor='gray' target='_blank'/>
         </div>
     )
